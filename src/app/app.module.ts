6b478321708be7709c0990bd0c03d5aff886d3a3
nimmy <nimmy2110@gmail.com>
2021-10-07 11:44:07 +0530

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialFormLoadComponent } from './formload/initial-form-load/initial-form-load.component';
+import { StudentDetailsComponent } from './sandra/student-details/student-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialFormLoadComponent,
+    StudentDetailsComponent,
   ],
   imports: [
     BrowserModule,
